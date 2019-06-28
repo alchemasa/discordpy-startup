5c46a045f0ec5fc2cecbd74058cfc45270d92068
alchemasa <52332335+alchemasa@users.noreply.github.com>
2019-06-29 07:15:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,7 +10,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
 
-@bot.command()
+@bot.event
 async def on_message(message):
     if message.content == '/pine':
 	pine = ['ウルフウルフ、どこ？今登った、登った！',
